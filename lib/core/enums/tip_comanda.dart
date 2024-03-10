839acd2c922a50a3b9067d08563af1a96c9873d7
Diana Stoica <diana.stoica@devplant.ro>
2024-03-10 18:14:46 +0200

    extensions on TipComanda enum

--- lib/core/enums/tip_comanda.dart
+++ lib/core/enums/tip_comanda.dart
@@ -1,1 +1,39 @@
-enum TipComanda { dinein, pickup }
+import 'package:flutter/material.dart';
+
+enum TipComanda { dinein, pickup, other }
+
+extension TipComandaExtension on TipComanda {
+  String get name {
+    switch (this) {
+      case TipComanda.dinein:
+        return 'Dine In';
+      case TipComanda.pickup:
+        return 'Pick Up';
+      default:
+        return 'Other';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case TipComanda.dinein:
+        return Icons.restaurant;
+      case TipComanda.pickup:
+        return Icons.shopping_bag;
+      default:
+        return Icons.more_horiz;
+
+    }
+  }
+
+  String get value {
+    switch (this) {
+      case TipComanda.dinein:
+        return 'dinein';
+      case TipComanda.pickup:
+        return 'pickup';
+      default:
+        return 'other';
+    }
+  }
+}

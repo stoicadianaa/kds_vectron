9b158ed009392bdbd2eeec8c2eeb982978141ec2
Diana Stoica <diana.stoica@devplant.ro>
2024-03-11 15:39:54 +0200

    center spinner

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "38c347fdfc808765d8c68ec564f4b8d4",
-"index.html": "00fd29529e72c393137c734df2df5169",
-"/": "00fd29529e72c393137c734df2df5169",
-"main.dart.js": "34138310c0ba943f450cfe9c2e32db27",
+"index.html": "5883760a08f8f26dff555cdb10266504",
+"/": "5883760a08f8f26dff555cdb10266504",
+"main.dart.js": "9d323af8720f7f14a139fcd2fad9e151",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",

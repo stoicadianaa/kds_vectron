bdc9774327d040c9d5cc205eafdeb95dd8985f3d
Diana Stoica <diana.stoica@devplant.ro>
2024-03-13 16:37:13 +0200

    remove unnecessary getAll call

--- lib/core/service/web_socket.service.dart
+++ lib/core/service/web_socket.service.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:kds_vectron/core/service/comenzi.service.dart';
@@ -8,9 +9,13 @@ import 'package:stomp_dart_client/stomp_config.dart';
 import 'package:kds_vectron/core/model/comanda.dart';
 
 class WebSocketService {
-  static final comandaUpdateController = StreamController<List<Comanda>>.broadcast();
+  static final comandaUpdateController =
+      StreamController<Map<String, Comanda>>.broadcast();
 
-  static Stream<List<Comanda>> get comandaUpdates => comandaUpdateController.stream;
+  static Stream<Map<String, Comanda>> get comandaUpdates =>
+      comandaUpdateController.stream;
+
+  static final Map<String, Comanda> _comenzi = {};
 
   static void connectToWebSocket() {
     log('Connecting to WebSocket...');
@@ -19,15 +24,26 @@ class WebSocketService {
       config: StompConfig(
         url: 'wss://vectron-kds-aad701792bc5.herokuapp.com/ws',
         onConnect: (StompFrame frame) async {
-          final List<Comanda> comenzi = await ComenziService().getComenzi();
-          comandaUpdateController.add(comenzi);
+          await ComenziService().getComenzi().then((value) {
+            final comenzi = <String, Comanda>{};
+            for (var comanda in value) {
+              comenzi[comanda.id] = comanda;
+            }
+            _comenzi.addAll(comenzi);
+            comandaUpdateController.add(comenzi);
+          });
 
           stompClient.subscribe(
             destination: '/topic/comandaUpdate',
             callback: (frame) async {
-              final List<Comanda> comenzi = await ComenziService().getComenzi();
-              comandaUpdateController.add(comenzi);
-              },
+              if (frame.body == null || frame.body!.isEmpty) {
+                return;
+              }
+              final Comanda comanda =
+                  Comanda.fromJson(jsonDecode(frame.body!)['comanda']);
+              _comenzi.addAll({comanda.id: comanda});
+              comandaUpdateController.add(_comenzi);
+            },
           );
         },
         onWebSocketError: (dynamic error) => log('WebSocket error: $error'),

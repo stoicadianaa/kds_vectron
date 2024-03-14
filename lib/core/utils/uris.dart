4868fe25a4ee61e51c9c83b05d227c9296a579cf
Diana Stoica <diana.stoica@devplant.ro>
2024-03-14 11:59:52 +0200

    change api url

--- lib/core/utils/uris.dart
+++ lib/core/utils/uris.dart
@@ -1,5 +1,5 @@
 class Uris {
-  static final Uri _baseUri = Uri.parse('https://vectron-kds-aad701792bc5.herokuapp.com');
+  static final Uri _baseUri = Uri.parse('https://kitchen-display-screen-7c99cf02ee68.herokuapp.com');
   static final Uri _baseWssUri = _baseUri.replace(scheme: 'wss');
 
   static final Uri comandaWsUri = _baseWssUri.replace(path: '/ws');
